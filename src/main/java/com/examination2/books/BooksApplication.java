a94ca677ffbc38158ec7e5d7699982f69a6362d5
Araki Takashi <ellaydelgado@gmail.com>
2024-01-20 00:11:27 +0900

    リポジトリ作成

---
+++ src/main/java/com/examination2/books/BooksApplication.java
@@ -0,0 +1,12 @@
+package com.examination2.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+}
