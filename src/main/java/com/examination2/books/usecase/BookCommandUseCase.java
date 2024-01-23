c72b58b6087adfa93fecf5728f8399881b978255
Araki Takashi <ellaydelgado@gmail.com>
2024-01-23 17:22:55 +0900

    BookCommandUseCaseにregisterBookメソッドの実装

---
+++ src/main/java/com/examination2/books/usecase/BookCommandUseCase.java
@@ -0,0 +1,16 @@
+package com.examination2.books.usecase;
+
+import com.examination2.books.domain.Book;
+import com.examination2.books.domain.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookCommandUseCase {
+   private final BookRepository bookRepository;
+
+   public Book registerBook(Book book) {
+       return bookRepository.register(book);
+   }
+}
